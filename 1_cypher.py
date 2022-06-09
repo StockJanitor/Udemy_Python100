72e4056c3f6ce94facd781b2a45f488a27f1eef1
https://stockjanitor@github.com <stockjanitor@gmail.com>
2022-06-09 00:12:12 -0700

    cypher

--- 1_cypher.py
+++ 1_cypher.py
@@ -10,4 +10,3 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-
