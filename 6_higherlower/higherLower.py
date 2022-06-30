ecc1b19268bada812a4db6ec69deb3b27b0fd48b
unknown <stockjanitor@gmail.com>
2022-06-29 18:29:39 -0700

    highlow

--- 6_higherlower/higherLower.py
+++ 6_higherlower/higherLower.py
@@ -1,3 +1,16 @@
 from game_data import data
+from random import randint
+
+def answer(a,b)
+    if a > b:
+        
+
+play = True
+while play:
+    item_list = data
+    n=len(data)
+    picked_item = randint(1,n)
+    item1 = item_list.pop(picked_item)
+
 
 print(len(data))
