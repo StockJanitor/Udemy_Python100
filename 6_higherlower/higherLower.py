7087293fc42f0333ea88223026676b0a5138efc2
https://stockjanitor@github.com <stockjanitor@gmail.com>
2022-06-30 21:45:24 -0700

    finish recursive

--- 6_higherlower/higherLower.py
+++ 6_higherlower/higherLower.py
@@ -11,51 +11,37 @@ def chose_item(x):
     a = x.pop(num1)
     return x, a
 
-
-def determine(x, y, z):
-    # x = user input, y = list, z = item
-
-    return
-
-
 list1, item1 = chose_item(data)
 
-
-def game():
+def game(list1, item1):
+    item1_name = item1["name"]
+    item1_follower = int(item1["follower_count"])
+    # chekc if you win
+    if len(data) == 0:
+        print("you win")
+        quit()
+    # print item 1
+    print(f"{item1_name} {item1_follower}")
+    # choose item 2
     list2, item2 = chose_item(data)
+    item2_name = item2["name"]
+    item2_follower = int(item2["follower_count"])
+    print(f"{item2_name}")
 
+    # evaluate answer
     answer = input("higher or lower? ")
-    if (answer == "higher") and (b > a):
-        chose_item(x, y)
-    elif (answer == "lower") and (b < a):
-        chose_item(x, y)
+    if (answer == "higher") and (item2_follower > item1_follower):
+        print("correct guess")
+        list1, item1 = list2, item2
+        game(list1, item1)
+        # game again
+    elif (answer == "lower") and (item2_follower < item1_follower):
+        print("correct guess")
+        list1, item1 = list2, item2
+        game(list1, item1)
     else:
         print("yoou loose")
-
-    # x.pop()
-    # print(a)
-    # print(len(x))
-    # b = x.pop[chose_num(x)]
-    # print(b)
-    # print(len(x))
+        quit()
 
 
-# return 2 selected item
-# answer
-# if correct >> game else end >>
-
-# def answer(a,b)
-#     if a > b:
-
-
-# play = True
-# while play:
-#     item_list = data
-#     n=len(data)
-#     picked_item = randint(1,n)
-#     item1 = item_list.pop(picked_item)
-
-
-# print(len(data))
-chose_item(data)
-print(len(data))
+game(list1, item1)
