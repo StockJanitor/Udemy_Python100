07fe9e115b499b468d76d4d5d311fc501bc2d58f
unknown <stockjanitor@gmail.com>
2022-07-06 18:46:59 -0700

    upload

---
+++ 7_coffeeMachine/coffee_machine.py
@@ -0,0 +1,33 @@
+# input - espresso, latte, cappuccino, off, report
+
+# if drink check resources
+
+# if resources process coins, if enough coins process, else need more coin
+
+# make more?
+
+water = 1000
+milk = 1000
+coffee = 500
+money = 0.00
+
+
+def check_resource(coffe):
+    '''type of coffee, resources'''
+    
+    # conditions 
+    if coffee == 'espresso':
+
+    elif coffee =='latte':
+    
+    elif coffee == 'cappuccino':
+        
+
+
+def machine():
+    '''run coffee machine'''
+
+    # action
+    action = input("What would you like? ")
+
+
