b216426b25f5991a43cdcd3b4ff34320c73d59dd
unknown <stockjanitor@gmail.com>
2022-07-12 13:47:46 -0700

    almost done

--- 7_coffeeMachine/coffee_machine.py
+++ 7_coffeeMachine/coffee_machine.py
@@ -1,5 +1,5 @@
-from calendar import c
-from pyrsistent import m
+# from calendar import c
+# from pyrsistent import m
 from coffee_data import MENU, resources
 
 list_menu = ["espresso", "latte", "cappuccino"]
@@ -19,8 +19,9 @@ coffee = resources["coffee"]
 money = 0.00
 
 
-def check_resource(coffe):
-    """type of coffee, resources"""
+def load_resource(coffee):
+    """input - type of coffee, output - resource amount"""
+    w,mk,c,m=0,0,0,0
     # conditions
     try:
         w = MENU[coffee]["ingredients"]["water"]
@@ -29,25 +30,59 @@ def check_resource(coffe):
         m = MENU[coffee]["cost"]
     except:
         pass
-
     return w, mk, c, m
 
 
 def machine():
     """run coffee machine"""
-
+    global water, milk, coffee
     # action
     action = input("What would you like? ")
     if action in list_menu:
-        check_resource(action)
+
+        # get amount of resources
+        w,mk,c,m = load_resource(action)
+
+        # reassign the new resources left
+        water = water -w
+        milk = milk-mk
+        coffee = coffee-c
+
+        #check resrouces
+        if (water<0) or (milk<0) or (coffee<0):
+            print("not enough resources")
+            quit()
+        
+        else:
+        # Process coins clause
+            quarters =int(input("how many quaters ? "))
+            dimes = int(input("how many dimes? "))
+            nickles = int(input("how many nickles? "))
+            pennies = int(input("how many pennies? "))
+            total = quarters*.25 + dimes*.1 + nickles*.05 +pennies*.01
+
+            if (total - money) >= 0:
+                print("drink processed")
+
+            else: 
+                pass
 
     elif action == "off":
         quit()
-    elif action == "report":
-        print()
-    else:
-        try:
-            pass
 
-        except:
-            pass
+    elif action == "report":
+        print(f"water :{water}")
+        print(f"milk :{milk}")
+        print(f"coffee :{coffee}")
+    
+    else:
+        print("invlaid command \n")
+        machine()
+
+    repeat= input("would you like to take action again? ")
+    if repeat =="yes":
+        machine()
+    else:
+        quit()
+
+machine()
