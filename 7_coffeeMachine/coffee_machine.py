1085f7e7809829834071c2dffd5878ab8648e133
https://stockjanitor@github.com <stockjanitor@gmail.com>
2022-07-11 23:50:11 -0700

    added function

--- 7_coffeeMachine/coffee_machine.py
+++ 7_coffeeMachine/coffee_machine.py
@@ -1,3 +1,9 @@
+from calendar import c
+from pyrsistent import m
+from coffee_data import MENU, resources
+
+list_menu = ["espresso", "latte", "cappuccino"]
+
 # input - espresso, latte, cappuccino, off, report
 
 # if drink check resources
@@ -6,28 +12,42 @@
 
 # make more?
 
-water = 1000
-milk = 1000
-coffee = 500
+water = resources["water"]
+milk = resources["milk"]
+coffee = resources["coffee"]
+
 money = 0.00
 
 
 def check_resource(coffe):
-    '''type of coffee, resources'''
-    
-    # conditions 
-    if coffee == 'espresso':
+    """type of coffee, resources"""
+    # conditions
+    try:
+        w = MENU[coffee]["ingredients"]["water"]
+        mk = MENU[coffee]["ingredients"]["milk"]
+        c = MENU[coffee]["ingredients"]["coffee"]
+        m = MENU[coffee]["cost"]
+    except:
+        pass
 
-    elif coffee =='latte':
-    
-    elif coffee == 'cappuccino':
-        
+    return w, mk, c, m
 
 
 def machine():
-    '''run coffee machine'''
+    """run coffee machine"""
 
     # action
     action = input("What would you like? ")
+    if action in list_menu:
+        check_resource(action)
 
+    elif action == "off":
+        quit()
+    elif action == "report":
+        print()
+    else:
+        try:
+            pass
 
+        except:
+            pass
