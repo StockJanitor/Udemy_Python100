88b7bdbb9393525dac9eac4c5adea287830ba862
unknown <stockjanitor@gmail.com>
2022-08-01 14:20:32 -0700

    add

--- 10_snake/main.py
+++ 10_snake/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 import random
+import time
 
 
 
@@ -8,8 +9,10 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
-starting_position = [(0,0),(-20,0),(-40,0)]
 
+
+
+starting_position = [(0,0),(-20,0),(-40,0)]
 segments = []
 
 for position in starting_position:
@@ -23,13 +26,14 @@ for position in starting_position:
 game_is_on= True
 while game_is_on:
     screen.update()
+    time.sleep(0.1)
     for seg_num in range(2, 0, -1):
         new_x = segments[seg_num -1].xcor()
         new_y = segments[seg_num -1].ycor()
         segments[seg_num].goto(new_x,new_y)
         
     segments[0].forward(20)
- 
+    segments[0].left(90)
 
 
 
