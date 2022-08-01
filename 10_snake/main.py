c2c9d6dd33cf381d9ec6cf1b8a6e2b3e3ba6632a
stockjanitor <stockjanitor@gmail.com>
2022-08-01 00:08:10 -0700

    add2lines

--- 10_snake/main.py
+++ 10_snake/main.py
@@ -7,7 +7,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-
+screen.tracer(0)
 starting_position = [(0,0),(-20,0),(-40,0)]
 
 segments = []
@@ -15,8 +15,16 @@ segments = []
 for position in starting_position:
     new_segment = Turtle("square")
     new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+screen.update()
 
-
+game_is_on= True
+while game_is_on:
+    screen.update()
+    for seg in segments:
+        seg.forward(20)
 
 
 
