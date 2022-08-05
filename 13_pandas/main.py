ab246d4a4836b2b7042757439a17018b2e84653a
unknown <stockjanitor@gmail.com>
2022-08-05 09:56:47 -0700

    upload

--- 13_pandas/main.py
+++ 13_pandas/main.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv(r"C:\Users\Gumo\Desktop\Git\Class\Udemy\13_pandas\weather_data.csv")
-
+df = pd.read_csv(r"C:\Users\Gumo\Desktop\Git\Class\Udemy\13_pandas\nato_phonetic_alphabet.csv")
 print(df)
+
+l1 = dict(zip(df.letter,df.code))
+print(l1)
+print("skip line here ~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+
+l2 = {row.letter:row.code for (index,row) in df.iterrows()}
+print(l2)
