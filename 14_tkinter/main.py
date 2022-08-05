c635d9c4797e6f3821278a97621bfa8f70a272ad
unknown <stockjanitor@gmail.com>
2022-08-05 10:27:10 -0700

    tkinter

---
+++ 14_tkinter/main.py
@@ -0,0 +1,18 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("first gui Program")
+window.minsize(width=500, height = 300)
+
+my_label = tkinter.Label(text="I am a label",font=("Arial",24,"bold"))
+my_label.pack()
+
+
+
+
+
+
+
+
+
+window.mainloop()
