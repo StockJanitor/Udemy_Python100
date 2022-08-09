07441244f7e84ef1868e0fbf793c022fca19ab87
unknown <stockjanitor@gmail.com>
2022-08-09 15:03:11 -0700

    email complete

--- 17_email/main.py
+++ 17_email/main.py
@@ -4,19 +4,27 @@ import random
 import smtplib
 from pass1 import password
 
+# variables
 my_email = "stockjanitor@yahoo.com"
 today = datetime.now()
 today_tuple=(today.month, today.day)
 
 path_data =r"C:\Users\Gumo\Desktop\Git\Class\Udemy\17_\birthdays.csv"
 path_letter = r"C:\Users\Gumo\Desktop\Git\Class\Udemy\17_\letter_templates\letter_"
+
 data = pd.read_csv(path_data)
+# create dictionary month, day: row of df
 birthdays_dict={(data_row["month"],data_row["day"]):data_row for (index,data_row)in data.iterrows()}
 
+# if today in birthday
 if today_tuple in birthdays_dict:
+
+    # get person info
     birthday_person = birthdays_dict[today_tuple]
-    file_path = f"{path_letter}{random.randint(1,3)}.txt"
-    with open(file_path) as letter_file:
+
+    # read file and replace NAME
+    path_file = f"{path_letter}{random.randint(1,3)}.txt"
+    with open(path_file) as letter_file:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
 
@@ -36,4 +44,4 @@ if today_tuple in birthdays_dict:
             msg=f"Subject: Happy Birthday!!\n\n{contents}"
             )
 
-# Python anywhere
+# Python anywhere.com
