24d26a2f676b7a79ae66b3f3ff762a3c21e89607
unknown <stockjanitor@gmail.com>
2022-08-08 17:14:11 -0700

    add path

--- 14_tkinter_timer/pomodoro.py
+++ 14_tkinter_timer/pomodoro.py
@@ -89,7 +89,11 @@ timer_label.grid(column=1,row=0)
 canvas = Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
 
 #import image
-path = r"C:\Users\Gumo\Desktop\Git\Class\Udemy\14_tkinter_timer\tomato.png"
+#surface path
+path=r"C:\Users\chenx\Desktop\Git\Class\Udemy_Python100\14_tkinter_timer\tomato.png"
+
+#desktop path
+# path = r"C:\Users\Gumo\Desktop\Git\Class\Udemy\14_tkinter_timer\tomato.png"
 tomato_img = PhotoImage(file=path)
 #insert image
 canvas.create_image(100,112,image=tomato_img)
