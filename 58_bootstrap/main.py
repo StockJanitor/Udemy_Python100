6f6d872ea28035bc5c3470528215a335dc9be175
unknown <stockjanitor@gmail.com>
2022-08-17 23:48:24 -0700

    pricing

--- 58_bootstrap/main.py
+++ 58_bootstrap/main.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_templates
 import datetime
+app = Flask()
 
 @app.route('/')
 def home():
     
     current_year = datetime.datetime.now().year
-    return render_template("index.html", year = current_year)
+    return render_templates("index.html", year = current_year)
