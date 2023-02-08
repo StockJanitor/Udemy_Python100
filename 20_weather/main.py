b29064e7e2164e18f9e786fbbeeceaa24013494c
unknown <stockjanitor@gmail.com>
2023-02-08 11:23:09 -0800

    sheety

--- 20_weather/main.py
+++ 20_weather/main.py
@@ -1,10 +1,10 @@
 #ventusky - live weather site
 
 import requests
-from twilio.rest import Client
+# from twilio.rest import Client
 
 # Load key
-path_token = r"C:\Users\Gumo\Desktop\Git\notebook\keys\openweather.txt" 
+path_token = r"C:\Users\Gumo\Desktop\Git\projects\notebook\keys\openweather.txt" 
 with open(path_token,"r") as newsfile:
     token = newsfile.readlines()
 
@@ -33,29 +33,30 @@ auth_token = ""
 response = requests.get(url,params=weather_parameter)
 response.raise_for_status()
 weather_data = response.json()
+print(weather_data)
 
-# parse out json
-weather_slice = weather_data["hourly"][:12]
+# # parse out json
+# weather_slice = weather_data["hourly"][:12]
 
-# determine if rain
-will_rain = False
+# # determine if rain
+# will_rain = False
 
-# loop list
-for hour_data in weather_slice:
-    condition_code = hour_data["weather"][0]["id"]
-    if int(600)<700:
-        will_rain = True
+# # loop list
+# for hour_data in weather_slice:
+#     condition_code = hour_data["weather"][0]["id"]
+#     if int(600)<700:
+#         will_rain = True
 
 
-# print out feedback
-if will_rain:
-    client = Client(account_sid, auth_token)
+# # print out feedback
+# if will_rain:
+#     client = Client(account_sid, auth_token)
 
-    message = client.messages \
-                .create(
-                     body="Its going to rain today",
-                     from_='+12133184231',
-                     to='+18184466008'
-                 )
+#     message = client.messages \
+#                 .create(
+#                      body="Its going to rain today",
+#                      from_='+12133184231',
+#                      to='+18184466008'
+#                  )
 
-    print(message.status)
+#     print(message.status)
